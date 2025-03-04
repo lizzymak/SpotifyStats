f04c714712b6c9c5c02ce86b34cdc46cd37ba0c4
lizzymak <146508325+lizzymak@users.noreply.github.com>
2025-03-03 21:57:13 -0800

    Delete SpotifyWrapped directory

--- SpotifyWrapped/js/index.js
+++
@@ -1,9 +0,0 @@
-const authEndPoint = 'https://accounts.spotify.com/authorize'
-const clientId = '6ce2e3aa41b949cc9806d3b53a39def4'
-const redirectUri = 'http://localhost:5500/html/callback.html'
-const scope = 'user-read-private user-read-email user-top-read user-read-recently-played'
-
-document.getElementById('loginButton').addEventListener('click', () =>{
-    const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${redirectUri}&scope=${scope}`
-    window.location.href = authUrl
-})
